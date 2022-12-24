b59d1e7a64c797979c0ee995423f8e1e2888dffc
victorsmith <victor7smith@gmail.com>
2022-12-23 21:06:37 -0700

    workout screen

--- screens/WorkoutsScreen.tsx
+++ screens/WorkoutsScreen.tsx
@@ -1,36 +1,37 @@
-import { RootTabScreenProps } from "../types";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import WorkoutDetailsScreen from "./WorkoutDetailsScreen";
-import WorkoutListScreen from "./WorkoutListScreen";
-import Workout from "../models/Workout";
-// import WorkoutComponent from "../components/Workout";
-import { ListRenderItem } from "react-native";
+import { RootTabScreenProps } from '../types';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import WorkoutDetailsScreen from './WorkoutDetailsScreen';
+import WorkoutListScreen from './WorkoutListScreen';
+import Workout from '../models/Workout';
+import { ListRenderItem } from 'react-native';
 
 export default function WorkoutsScreen({
-	navigation,
-}: RootTabScreenProps<"Workouts">) {
-	const renderItem: ListRenderItem<Workout> = ({ item }) => {
-		return <WorkoutScreenNavigator />;
-	};
+  navigation,
+}: RootTabScreenProps<'Workouts'>) {
+  const renderItem: ListRenderItem<Workout> = ({ item }) => {
+    return <WorkoutScreenNavigator />;
+  };
+}
 
 interface WorkoutDetailsParams {
-	workout: Workout;
+  workout: Workout;
 }
+
 export type WorkoutStackParamList = {
-	WorkoutList: undefined;
-	WorkoutDetails: WorkoutDetailsParams;
+  WorkoutList: undefined;
+  WorkoutDetails: WorkoutDetailsParams;
 };
 
 const WorkoutStack = createNativeStackNavigator<WorkoutStackParamList>();
 
 function WorkoutScreenNavigator() {
-	return (
-		<WorkoutStack.Navigator initialRouteName="WorkoutList">
-			<WorkoutStack.Screen name="WorkoutList" component={WorkoutListScreen} />
-			<WorkoutStack.Screen
-				name="WorkoutDetails"
-				component={WorkoutDetailsScreen}
-			/>
-		</WorkoutStack.Navigator>
-	);
+  return (
+    <WorkoutStack.Navigator initialRouteName="WorkoutList">
+      <WorkoutStack.Screen name="WorkoutList" component={WorkoutListScreen} />
+      <WorkoutStack.Screen
+        name="WorkoutDetails"
+        component={WorkoutDetailsScreen}
+      />
+    </WorkoutStack.Navigator>
+  );
 }
