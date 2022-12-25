3c31d5f49f55331155a7ca7146edf887e08d96e8
victorsmith <victor7smith@gmail.com>
2022-12-24 18:11:35 -0700

    settings general structure

--- screens/SettingsTab/SettingsScreen.tsx
+++ screens/SettingsTab/SettingsScreen.tsx
@@ -1,3 +1,30 @@
 // export default SettingsScreen({navigation,} : RootTabScreenProps<'Settings'>) {
 //   return <SettingsScreenNavigator/>;/*  */
 // }
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+
+export default function SettingsScreen({ navigation } : RootTabScreenProps<'Settings'>) {
+  return <SettingsScreenNavigator/>
+}
+
+interface SettingsDetailsParam {
+  // setting: Settings,
+}
+
+export type SettingsStackParamList = {
+  WorkoutList: undefined;
+  WorkoutDetails: SettingsDetailsParam;
+};
+
+const SettingsStack = createNativeStackNavigator<SettingsStackParamList>()
+
+function SettingsScreenNavigator() {
+  return(
+    <SettingsStack.Navigator>
+        <SettingsStack.Screen></SettingsStack.Screen>
+    </SettingsStack.Navigator>
+  )
+
+}
