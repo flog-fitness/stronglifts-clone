a3048f9b3b8fa5b2aa6237ff5cb103bf1f08a800
victorsmith <victor7smith@gmail.com>
2023-01-02 14:30:10 -0700

    added setting sub screens

---
+++ screens/SettingsTab/Settings/WeightUnitScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function WeightUnitScreen() {
+  return (
+    <View>
+      <Text>WeightUnitScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
