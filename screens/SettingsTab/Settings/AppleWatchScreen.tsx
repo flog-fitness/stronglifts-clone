0fbeb7fe05d3031c20217deda6ae9003e2dbf019
victorsmith <victor7smith@gmail.com>
2023-01-02 14:34:25 -0700

    added template ui for stack pages

---
+++ screens/SettingsTab/Settings/AppleWatchScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function AppleWatchScreen() {
+  return (
+    <View>
+      <Text>AppleWatchScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
