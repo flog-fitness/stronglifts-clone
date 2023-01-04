41446a8533c07c6fd0bcd7a1b45269ddb66f7975
ChrisSyrnyk <stuntdouble@me.com>
2023-01-04 14:15:00 -0700

    History page/Calendar + notes tab

--- screens/HistoryScreen.tsx
+++ screens/HistoryScreen.tsx
@@ -1,31 +1,145 @@
-import { StyleSheet } from 'react-native';
+import { 
+  SafeAreaView,
+  StyleSheet,
+  View, 
+  Text, 
+  TextInput,
+  Keyboard} from "react-native";
+import { RootTabScreenProps } from "../types";
+import {CalendarList} from 'react-native-calendars';
+import {useState} from 'react';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+//example of how to mark stuff
+//We should add a date field to workouts and pull data from there
+const marked = {
+  '2023-01-10': { marked: true },
+  '2023-01-12': { selected: true },
+};
 
-export default function HistoryScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>History</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
-  );
+export default function HistoryScreen({
+navigation,
+}: RootTabScreenProps<"History">) {
+
+  const [DisplayedTab, setDisplayedTab] = useState('calendar'); //calendar tab displayed as default
+  const [text, onChangeText] = useState("notes...");
+  
+  //tabs for List/Calendar/Notes.
+  if (DisplayedTab == 'calendar'){
+      return (
+          <View style={styles.historyContainer}>
+              <View style = {styles.selectionDiv}>
+                      <Text style = {styles.unselectedText} onPress = {()=> setDisplayedTab('list')}>{'List'}</Text>
+                      <Text style = {styles.selectedText} onPress = {()=> setDisplayedTab('calendar')}>{'Calendar'}</Text>
+                      <Text style = {styles.unselectedText} onPress = {()=> setDisplayedTab('notes')}>{'Notes'}</Text>
+              </View>
+              <SafeAreaView style={styles.calendarWrapper}>
+                  <CalendarList   //there is an error with the calendars current date. It marks the current day on every month...
+                      style={{
+                          borderWidth: 10,
+                          borderColor: 'black',
+                      }}
+                      theme={{
+                          calendarBackground: 'black',
+                          dayTextColor: '#fff',
+                          textDisabledColor: '#444',
+                          monthTextColor: '#888',
+                      }}
+                      markedDates = {marked}  //display marked dates
+                  />
+              </SafeAreaView> 
+          </View>
+      );
+  } else if (DisplayedTab === 'notes'){
+      return (
+          <View style={styles.historyContainer}> 
+              <View style = {styles.selectionDiv}>
+                      <Text style = {styles.unselectedText} onPress = {()=> setDisplayedTab('list')}>{'List'}</Text>
+                      <Text style = {styles.unselectedText} onPress = {()=> setDisplayedTab('calendar')}>{'Calendar'}</Text>
+                      <Text style = {styles.selectedText} onPress = {()=> setDisplayedTab('notes')}>{'Notes'}</Text>
+              </View> 
+              <TextInput
+                  style={styles.input}
+                  onChangeText={onChangeText}
+                  keyboardType={'default'}
+                  keyboardAppearance={'dark'}
+                  value={text}
+                  multiline={true}
+                  onSubmitEditing={Keyboard.dismiss}
+              />
+          </View>
+      );
+  } else if (DisplayedTab === 'list'){
+      return (
+          <View style={styles.historyContainer}> 
+              <View style = {styles.selectionDiv}>
+                      <Text style = {styles.selectedText} onPress = {()=> setDisplayedTab('list')}>{'List'}</Text>
+                      <Text style = {styles.unselectedText} onPress = {()=> setDisplayedTab('calendar')}>{'Calendar'}</Text>
+                      <Text style = {styles.unselectedText} onPress = {()=> setDisplayedTab('notes')}>{'Notes'}</Text>
+              </View>
+              <View style = {{paddingTop: 100}}>
+                  <Text style = {{color: 'white',}}>Undeveloped</Text>
+              </View>
+          </View>
+      );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+historyContainer: { //page container
+  flex: 1,
+  alignItems: "center",
+  paddingTop: 1,
+  paddingHorizontal: 20,
+      backgroundColor: 'black',
+},
+  selectionDiv: { //selection header container
+      flexDirection: 'row',
+      width: '100%',
+      height: 30,
+      margin: 10,
+      marginLeft: 5,
+      marginRight: 5,
+      marginTop: 60,
+      justifyContent: 'space-between',
+      backgroundColor: '#5A5A5A',
+      borderRadius: 10,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  unselectedText: { //selection text container (unselected)
+      width: '33%',
+      height: 'auto',
+      backgroundColor: '#5A5A5A',
+      color: 'white',
+      fontWeight: 'bold',
+      padding: 6,
+      borderRadius: 10,
+      textAlign: 'center',
+      overflow: 'hidden',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  selectedText: { //could probably change to just a background color and add to styles of selected text
+      width: '33%',
+      height: 'auto',
+      backgroundColor: '#a9a9a9',
+      color: 'white',
+      fontWeight: 'bold',
+      padding: 6,
+      borderRadius: 10,
+      borderColor: 'grey',
+      textAlign: 'center',
+      overflow: 'hidden',
+  },
+  calendarWrapper:{
+      height: '100%', 
+      width: 400,
+      justifyContent: "center",    
+  },
+  input: {
+      backgroundColor: '#5A5A5A',
+      color: 'white',
+      height: '30%', 
+      width: 350,
+      margin: 'auto',
+      borderRadius: 10,
+      padding: 10,
+      overflow: 'hidden'
   },
 });
